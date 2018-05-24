1f6d25d17001351bd3b68f0020bad3c62ea3681b
Shikirra Williams <shikirra.williams@gmail.com>
2018-05-24 11:25:23 -0400

    connected to database

--- appt/src/main/java/com/example/appt/appointmentapp/AppointmentController.java
+++ appt/src/main/java/com/example/appt/appointmentapp/AppointmentController.java
@@ -1,41 +1,37 @@
 package com.example.appt.appointmentapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Scanner;
 
-import java.sql.Time;
-import java.util.Date;
-
-@Controller
 public class AppointmentController {
 
-    @Autowired
-    AppointmentRepository appointmentRepository;
+//    @Autowired
+//    AppointmentRepository appointmentRepository;
 
-    @PostMapping("/")
-    public String appointmentForm (@RequestBody Appointment appointment){
-
-        Date  apptDate = appointment.getDate();
-
-        Time  apptTime = appointment.getTime();
-
-        String apptDescription = appointment.getDescriptionOfAppt();
-
-        Appointment createNewAppointment = new Appointment();
-
-        createNewAppointment.setDate(apptDate);
-        createNewAppointment.setTime(apptTime);
-
-        appointmentRepository.add(createNewAppointment);
-
+//    @PostMapping("/")
+//    public String appointmentForm (@RequestBody Appointment appointment){
+//
+//        Date  apptDate = appointment.getDate();
+//
+//        Time  apptTime = appointment.getTime();
+//
+//        String apptDescription = appointment.getDescriptionOfAppt();
+//
+//        Appointment createNewAppointment = new Appointment();
+//
+//        createNewAppointment.setDate(apptDate);
+//        createNewAppointment.setTime(apptTime);
+//
+//        appointmentRepository.add(createNewAppointment);
+//
 //        if (apptDate == null || apptTime == null || apptDescription == null){
 //            return "please fill in each field";
 //        } else if (){
 //
 //        }
-            return "new appointment was created";
+//            return "new appointment was created";
+//
+//    }
+    Scanner scanner = new Scanner(System.in);
 
-    }
+    String searchInput = scanner.hasNext(#search);
 }
